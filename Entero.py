b80c92d6256bd9fb585a85229cf08adb57afb685
ism47968960 <ism47968960@a02.informatica.escoladeltreball.org>
2018-01-24 12:05:58 +0100

    first commit

---
+++ Entero.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+
+
+divisor=int(input("Introduzca el divisor: "))
+dividendo=int(input("Introduzca el dividendo: "))
+
+if divisor%dividendo == 0:
+	print "Correcto"
+else:
+	print "Incorrecto"
+
+
+
+
